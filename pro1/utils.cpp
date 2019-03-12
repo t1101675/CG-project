23f16dd7d709be02d7f3c243232fa62466a34d00
顾煜贤 <2017011421@secoder.net>
2019-03-12 18:27:12 +0800

    compele code for fill begin debugging

--- pro1/utils.cpp
+++ pro1/utils.cpp
@@ -1,5 +1,10 @@
-#include "utils.h"
+#include <list>
+#include <assert>
+#include <vector>
 #include <iostream>
+
+
+#include "utils.h"
 
 inline int abs(int a) { return a > 0 ? a : -a; }
 
@@ -8,7 +13,6 @@ void drawLine(int x1, int y1, int x2, int y2) {
   int ux = dx > 0 ? 1 : -1, uy = dy > 0 ? 1 : -1;
   int dxAbs = abs(dx), dyAbs = abs(dy);
   int dxAbs2 = dxAbs << 1, dyAbs2 = dyAbs << 1;
-  // std::cout << dxAbs2 << " " << dyAbs2 << std::endl;
   if (dxAbs > dyAbs) {
     int e = 0;
     for (int x = x1, y = y1; x != x2; x += ux) {
@@ -18,7 +22,6 @@ void drawLine(int x1, int y1, int x2, int y2) {
         y += uy;
         e -= dxAbs2;
       }
-      std::cout << x << " " << y << std::endl;
     }
   }
   else {
@@ -33,5 +36,69 @@ void drawLine(int x1, int y1, int x2, int y2) {
     }
   }
   glVertex2i(x2, y2);
-  std::cout << x2 << " " << y2 << std::endl;
+}
+
+void fill(const std::vector<Point2>& vp) {
+  if (vp.size() <= 0) return;
+  int yMin = 0x7fffffff;
+  int yMax = yMin + 1;
+  for (int i = 0; i < vp.size(); i++) {
+    if (vp[i].y > yMax) yMax = vp[i].y;
+    if (vp[i].y < yMin) yMin = vp[i].y;
+  }
+
+  std::list<ETNode>* NET = new std::list<ETNode>[yMax - yMin + 1];
+  for (int y = yMin; y <= yMax; y++) {
+    for (int i = 0; i < vp.size(); i++) {
+      if (vp[i].y == y) {
+        int prev = i > 0 ? i - 1 : vp.size();
+        if (vp[prev].y > y) {
+          int dy = vp[prev].y - vp[i].y;
+          NET.push_back(ETNode(vp[i].x * (dy ? dy : 1),
+                              (vp[prev].x - vp[i].x),
+                              dy,
+                              vp[prev].y));
+        }
+        int post = i + 1 < vp.size() ? i + 1 : 0;
+        if (vp[post].y > y) {
+          int dy = vp[post].y - vp[i].y;
+          NET.push_back(ETNode(vp[i].x * (dy ? dy : 1),
+                              (vp[post].x - vp[i].x),
+                              dy,
+                              vp[post].y));
+        }
+      }
+    }
+  }
+
+  std::list<ETNode> AET;
+  for (int y = yMin; y <= yMax; y++) {
+    std::list<ETNode>::iterator itNET = NET.begin(), itAET = AET.begin();
+    for (; itNET != NET.end(); ++itNET) {
+      while (itAET != AET.end() && *itAET.x < *itNET.x) {
+        itAET++;
+      }
+      AET.insert(itAET, *itNET);
+    }
+
+    assert(AET.size() % 2 == 0);
+
+    itAET = AET.begin();
+    while (itAET != AET.end()) {
+      if (*itAET.y == 0) xMin = (*itAET).x;
+      int xMin = (*itAET).x / (*itAET).dy;
+      itAET++;
+      if (*itAET.y == 0) xMax = (*itAET).x;
+      int xMax = (*itAET).x / (*itAET).dy;
+      itAET++;
+      for (int x = xMin; x <= xMax; x++) {
+        glVertex2i(x, y);
+      }
+    }
+
+    for (itAET = AET.begin(); itAET != AET.end(); ++itAET) {
+      if ((*itAET).yMax <= y) AET.erase(itAET);
+      else (*itAET).x += (*itAET).dy ? (*itAET).dx : 0;
+    }
+  }
 }
--- pro1/utils.h
+++ pro1/utils.h
@@ -1,3 +1,20 @@
+#include <vector>
 #include <GL/glut.h>
 
+struct Point2 {
+  int x, y;
+  Point(int xx = 0, int yy = 0): x(xx), y(yy) {}
+};
+
+struct ETNode {
+  int x;
+  int dx;
+  int dy;
+  int yMax;
+
+  ETNode(int xx = 0, int ddx = 0, int ddy = 0, int yyMax = 0): x(xx), dx(ddx), dy(ddy), yMax(yyMax) {}
+};
+
 void drawLine(int x1, int y1, int x2, int y2);
+
+void fill(const std::vector<Point2>& vp);
